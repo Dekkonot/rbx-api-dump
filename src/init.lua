afa2d35ea01016e1c76abeffab1d5df8c133d4ec
Dekkonot <4625546+Dekkonot@users.noreply.github.com>
2020-12-25 14:16:08 -0800

    Remove unnecessary whitespace

--- src/init.lua
+++ src/init.lua
@@ -29,17 +29,17 @@ local cloneMember = Util.cloneMember
 
 local function tryAPI(): ()
 	if dump then return end
-	
+
 	dump = FetchApi()
-	
+
 	if dump.Version ~= 1 then
 		error(string.format(INVALID_DUMP_VERSION, dump.Version), 2)
 	end
-	
+
 	for _, class in ipairs(dump.Classes) do
 		classMap[class.Name] = class
 	end
-	
+
 	for className in pairs(classMap) do
 		local classTables = {}
 		local root = className
@@ -64,25 +64,25 @@ function API.getMembers(class: string, tagFilter: Array<string>?, securityFilter
 	if not dump then
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
-	
+
 	local superClasses: Array<ApiTypes.Class> = superClassMap[class]
 	if not superClasses then
 		error(string.format(CLASS_NOT_REAL_MESSAGE, class), 2)
 	end
-	
+
 	local tagLookup: Dictionary<boolean> = lookupify(tagFilter)
 	local securityLookup: Dictionary<boolean> = lookupify(securityFilter)
-	
+
 	local memberList: Dictionary<ApiTypes.Member> = {}
 	for _, class in ipairs(superClasses) do
 		for _, v in ipairs(class.Members) do
 			if filterSecurity(v.Security, securityLookup) then continue end
 			if filterTags(v.Tags, tagLookup) then continue end
-			
+
 			memberList[v.Name] = cloneMember(v)
 		end
 	end
-	
+
 	return memberList
 end
 
@@ -90,26 +90,26 @@ function API.getProperties(class: string, tagFilter: Array<string>?, securityFil
 	if not dump then
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
-	
+
 	local superClasses: Array<ApiTypes.Class> = superClassMap[class]
 	if not superClasses then
 		error(string.format(CLASS_NOT_REAL_MESSAGE, class), 2)
 	end
-	
+
 	local tagLookup: Dictionary<boolean> = lookupify(tagFilter)
 	local securityLookup: Dictionary<boolean> = lookupify(securityFilter)
-	
+
 	local memberList: Dictionary<ApiTypes.Property> = {}
 	for _, class in ipairs(superClasses) do
 		for _, v in ipairs(class.Members) do
 			if v.MemberType ~= "Property" then continue end
 			if filterSecurity(v.Security, securityLookup) then continue end
 			if filterTags(v.Tags, tagLookup) then continue end
-			
+
 			memberList[v.Name] = cloneMember(v)
 		end
 	end
-	
+
 	return memberList
 end
 
@@ -117,26 +117,26 @@ function API.getFunctions(class: string, tagFilter: Array<string>?, securityFilt
 	if not dump then
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
-	
+
 	local superClasses: Array<ApiTypes.Class> = superClassMap[class]
 	if not superClasses then
 		error(string.format(CLASS_NOT_REAL_MESSAGE, class), 2)
 	end
-	
+
 	local tagLookup: Dictionary<boolean> = lookupify(tagFilter)
 	local securityLookup: Dictionary<boolean> = lookupify(securityFilter)
-	
+
 	local memberList: Dictionary<ApiTypes.Function> = {}
 	for _, class in ipairs(superClasses) do
 		for _, v in ipairs(class.Members) do
 			if v.MemberType ~= "Function" then continue end
 			if filterSecurity(v.Security, securityLookup) then continue end
 			if filterTags(v.Tags, tagLookup) then continue end
-			
+
 			memberList[v.Name] = cloneMember(v)
 		end
 	end
-	
+
 	return memberList
 end
 
@@ -144,26 +144,26 @@ function API.getEvents(class: string, tagFilter: Array<string>?, securityFilter:
 	if not dump then
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
-	
+
 	local superClasses: Array<ApiTypes.Class> = superClassMap[class]
 	if not superClasses then
 		error(string.format(CLASS_NOT_REAL_MESSAGE, class), 2)
 	end
-	
+
 	local tagLookup: Dictionary<boolean> = lookupify(tagFilter)
 	local securityLookup: Dictionary<boolean> = lookupify(securityFilter)
-	
+
 	local memberList: Dictionary<ApiTypes.Event> = {}
 	for _, class in ipairs(superClasses) do
 		for _, v in ipairs(class.Members) do
 			if v.MemberType ~= "Event" then continue end
 			if filterSecurity(v.Security, securityLookup) then continue end
 			if filterTags(v.Tags, tagLookup) then continue end
-			
+
 			memberList[v.Name] = cloneMember(v)
 		end
 	end
-	
+
 	return memberList
 end
 
@@ -171,26 +171,26 @@ function API.getCallbacks(class: string, tagFilter: Array<string>?, securityFilt
 	if not dump then
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
-	
+
 	local superClasses: Array<ApiTypes.Class> = superClassMap[class]
 	if not superClasses then
 		error(string.format(CLASS_NOT_REAL_MESSAGE, class), 2)
 	end
-	
+
 	local tagLookup: Dictionary<boolean> = lookupify(tagFilter)
 	local securityLookup: Dictionary<boolean> = lookupify(securityFilter)
-	
+
 	local memberList: Dictionary<ApiTypes.Callback> = {}
 	for _, class in ipairs(superClasses) do
 		for _, v in ipairs(class.Members) do
 			if v.MemberType ~= "Callback" then continue end
 			if filterSecurity(v.Security, securityLookup) then continue end
 			if filterTags(v.Tags, tagLookup) then continue end
-			
+
 			memberList[v.Name] = cloneMember(v)
 		end
 	end
-	
+
 	return memberList
 end
 
@@ -198,17 +198,17 @@ function API.getSuperclasses(class: string): Array<string>
 	if not dump then
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
-	
+
 	local superClasses: Array<ApiTypes.Class> = superClassMap[class]
 	if not superClasses then
 		error(string.format(CLASS_NOT_REAL_MESSAGE, class), 2)
 	end
-	
+
 	local list = {}
 	for i, class in ipairs(superClasses) do
 		list[i] = class.Name
 	end
-	
+
 	return list
 end
 
@@ -216,14 +216,14 @@ function API.getTags(class: string, member: string?): Array<string>
 	if not dump then
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
-	
+
 	local classTable: ApiTypes.Class = classMap[class]
 	if not classTable then
 		error(string.format(CLASS_NOT_REAL_MESSAGE, class), 2)
 	end
-	
+
 	local tagList: Array<string> = {}
-	
+
 	if member then
 		for _, v in ipairs(classTable.Members) do
 			if v.Name == member then
@@ -251,12 +251,12 @@ function API.isDeprecated(class: string, member: string?): boolean
 	if not dump then
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
-	
+
 	local classTable: ApiTypes.Class = classMap[class]
 	if not classTable then
 		error(string.format(CLASS_NOT_REAL_MESSAGE, class), 2)
 	end
-	
+
 	if member then
 		local members = API.getMembers(class, {"Deprecated"})
 		if members[member] then
@@ -279,12 +279,12 @@ function API.isService(class: string): boolean
 	if not dump then
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
-	
+
 	local classTable: ApiTypes.Class = classMap[class]
 	if not classTable then
 		error(string.format(CLASS_NOT_REAL_MESSAGE, class), 2)
 	end
-	
+
 	local tags: typeof(classTable.Tags) = classTable.Tags
 	if tags then
 		if table.find(tags, "Service") then
@@ -299,18 +299,18 @@ function API.getClasses(filter: Array<string>?): Array<string>
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
 	local classList: Array<string> = {}
-	
+
 	local tagLookup: Dictionary<boolean> = lookupify(filter)
-	
+
 	local classCount = 1
-	
+
 	for _, v in ipairs(dump.Classes) do
 		if filterTags(v.Tags, tagLookup) then continue end
-		
+
 		classList[classCount] = v.Name
 		classCount += 1
 	end
-	
+
 	return classList
 end
 
@@ -319,18 +319,18 @@ function API.getEnums(filter: Array<string>?): Array<string>
 		error(MODULE_NOT_READY_MESSAGE, 2)
 	end
 	local enumList: Array<string> = {}
-	
+
 	local tagLookup: Dictionary<boolean> = lookupify(filter)
-	
+
 	local enumCount = 1
-	
+
 	for _, v in ipairs(dump.Enums) do
 		if filterTags(v.Tags, tagLookup) then continue end
-		
+
 		enumList[enumCount] = v.Name
 		enumCount += 1
 	end
-	
+
 	return enumList
 end
 
@@ -345,9 +345,9 @@ coroutine.resume(coroutine.create(function()
 			if accum <= step then return end
 			accum = 0
 			step *= 2
-			
+
 			succ, err = pcall(tryAPI)
-			
+
 			if succ then
 				tryConnection:Disconnect()
 				ReadyBindable:Fire()
